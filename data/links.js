a76aa1afa962f31fd89a28a39b27241c80de90d9
elifoster <elifosterwy@gmail.com>
2016-11-06 11:16:33 -0800

    :lock: :racehorse: Improve security and performance by not messign with innerHTML at all. This was suggested by Mozilla Addon Reviewier Rob W.

--- data/links.js
+++ data/links.js
@@ -1,38 +1,28 @@
 /**
- * Emits a request to get the decoded URL for the provided name.
- * For example: FTB-Gamepedia/MediaWiki-Butt-Ruby:master returns https://github.com/FTB-Gamepedia/MediaWiki-Butt-Ruby/tree/master
+ * @param fullName The title name for the branch <repository>:<branch>.
+ * @return string The URL for the repo and branch title.
+ *                For example: FTB-Gamepedia/MediaWiki-Butt-Ruby:master returns https://github.com/FTB-Gamepedia/MediaWiki-Butt-Ruby/tree/master
  */
-function getURL(fullName, index) {
+function getURL(fullName) {
     var twoElements = fullName.split(':');
-    self.port.emit('getDecoded', { str: `https://github.com/${twoElements[0]}/tree/${twoElements[1]}`, index: index });
+    return `https://github.com/${twoElements[0]}/tree/${twoElements[1]}`;
 }
 
 var commitRefs = document.querySelectorAll('.commit-ref');
 
-/**
- * Performs a function for every found commit ref after it checks if the element's title is null.
- * @param funcToDo The function to perform after the null check on each individual element. The function takes the title
- *        value and the index of the element as parameters.
- */
-function doForEachCommitRef(funcToDo) {
-    for (var i = 0; i < commitRefs.length; i++) {
-        var titleItem = commitRefs[i].attributes.getNamedItem('title');
-        // "unknown repository"- forker deleted their repo.
-        // Unfortunately there is nothing like this for deleted branches so those will simply take you to a 404.
-        if (!titleItem) {
-            continue;
-        }
-        funcToDo(titleItem.value, i);
+for (let element of commitRefs) {
+    // "unknown repository"- forker deleted their repo.
+    // Unfortunately there is nothing like this for deleted branches so those will simply take you to a 404.
+    if (!element.title) {
+        continue;
     }
+
+    let a = document.createElement('a');
+    a.href = getURL(element.title);
+
+    while (element.hasChildNodes()) {
+        a.appendChild(element.firstChild);
+    }
+    
+    element.appendChild(a);
 }
-
-doForEachCommitRef((function(title, index) {
-    getURL(title, index);
-}));
-
-self.port.on('returnDecoded', function(obj) {
-    var decodedStr = obj['str'];
-    var element = commitRefs[obj['index']];
-    var oldInnerHTML = element.innerHTML;
-    element.innerHTML = `<a href="${decodedStr}">${oldInnerHTML}</a>`;
-});
--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 var self = require('sdk/self');
 var pageMod = require('sdk/page-mod');
-var he = require('he');
 
 pageMod.PageMod({
     include: /https?:\/\/(www\.)?github\.com(\/.*)?/,
     contentScriptFile: self.data.url('links.js'),
-    attachTo: 'top',
-    onAttach: startListening
+    attachTo: 'top'
 });
-
-function startListening(worker) {
-    worker.port.on('getDecoded', function(obj) {
-        obj['str'] = he.decode(obj['str']);
-        worker.port.emit('returnDecoded', obj);
-    });
-}
